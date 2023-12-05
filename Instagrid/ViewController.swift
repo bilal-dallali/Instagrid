179b0cdafa343dcfc4cb18e53d6997fcef926911
bilal-dallali <82528588+bilal-dallali@users.noreply.github.com>
2023-12-05 18:19:32 +0100

    layer1 middleview done

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -14,13 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var selected2: UIImageView!
     @IBOutlet weak var selected3: UIImageView!
     
+    @IBOutlet weak var middleView1: UIView!
+    @IBOutlet weak var middleView2: UIView!
+    @IBOutlet weak var middleView3: UIView!
+    @IBOutlet weak var middleView4: UIView!
+    
+    @IBOutlet weak var plusMiddleView1: UIImageView!
+    @IBOutlet weak var plusMiddleView2: UIImageView!
+    @IBOutlet weak var plusMiddleView3: UIImageView!
+    @IBOutlet weak var plusMiddleView4: UIImageView!
+    
+    @IBOutlet weak var plusMiddleViewTop: UIImageView!
+    @IBOutlet weak var plusMiddleViewBottom: UIImageView!
+    
+    
     @IBAction func selectLayout1(_ sender: UIButton) {
         //sender.isSelected = true
         //sender.isSelected.toggle()
         selected1.layer.opacity = 1
         selected2.layer.opacity = 0
         selected3.layer.opacity = 0
-        
+        setLayer1()
     }
     
     @IBAction func selectLayout2(_ sender: UIButton) {
@@ -39,8 +53,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLayer1()
+    }
+    
+    func setLayer1() {
         selected2.layer.opacity = 0
         selected3.layer.opacity = 0
+        middleView1.frame = CGRect(x: middleView1.frame.origin.x, y: middleView1.frame.origin.y, width: 270, height: middleView1.frame.size.height)
+        middleView2.layer.opacity = 0
+        plusMiddleView1.layer.opacity = 0
+        plusMiddleViewBottom.layer.opacity = 0
     }
     
 }
