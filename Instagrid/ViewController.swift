76257b394ed8109e040ef7f1fab85bdb9ccc9896
bilal-dallali <82528588+bilal-dallali@users.noreply.github.com>
2023-12-05 15:49:06 +0100

    you can select the icon

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -14,13 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var selected2: UIImageView!
     @IBOutlet weak var selected3: UIImageView!
     
+    @IBAction func selectLayout1(_ sender: UIButton) {
+        //sender.isSelected = true
+        //sender.isSelected.toggle()
+        selected1.layer.opacity = 1
+        selected2.layer.opacity = 0
+        selected3.layer.opacity = 0
+        
+    }
+    
+    @IBAction func selectLayout2(_ sender: UIButton) {
+        selected1.layer.opacity = 0
+        selected2.layer.opacity = 1
+        selected3.layer.opacity = 0
+    }
+    
+    @IBAction func selectLayout3(_ sender: UIButton) {
+        selected1.layer.opacity = 0
+        selected2.layer.opacity = 0
+        selected3.layer.opacity = 1
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         selected2.layer.opacity = 0
         selected3.layer.opacity = 0
-
     }
-
-
+    
 }
