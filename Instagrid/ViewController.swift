08e7c2a2cac5053627648a66c1d365263468dc83
Bilal D <bilal@MacBook-Pro-de-Bilal.local>
2023-12-18 19:31:53 +0100

    tap gesture recognizer

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 //import PhotosUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    
     
     @IBOutlet weak var selected1: UIImageView!
     @IBOutlet weak var selected2: UIImageView!
@@ -19,6 +21,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var middleView3: UIView!
     @IBOutlet weak var middleView4: UIView!
     
+    var currentImage1 = UIImage.self
+    
     @IBOutlet weak var plusMiddleView1: UIImageView!
     @IBOutlet weak var plusMiddleView2: UIImageView!
     @IBOutlet weak var plusMiddleView3: UIImageView!
@@ -27,6 +31,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var plusMiddleViewTop: UIImageView!
     @IBOutlet weak var plusMiddleViewBottom: UIImageView!
     
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     
     @IBAction func selectLayout1(_ sender: UIButton) {
         //sender.isSelected = true
@@ -51,6 +60,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setLayer1()
+        
+        setupTapGesture(for: middleView1, action: #selector(middleView1Tapped))
+        setupTapGesture(for: middleView2, action: #selector(middleView2Tapped))
+        setupTapGesture(for: middleView3, action: #selector(middleView3Tapped))
+        setupTapGesture(for: middleView4, action: #selector(middleView4Tapped))
+    }
+    
+    func setupTapGesture(for view: UIView, action: Selector) {
+        let tapGesture = UITapGestureRecognizer(target: self, action: action)
+        view.addGestureRecognizer(tapGesture)
+        view.isUserInteractionEnabled = true
+    }
+    
+    @objc func middleView1Tapped() {
+        print("middleView1 a été tapé")
+        // Ajoutez ici le code pour gérer le tap sur middleView1
+    }
+    
+    @objc func middleView2Tapped() {
+        print("middleView2 a été tapé")
+        // Ajoutez ici le code pour gérer le tap sur middleView2
+    }
+    
+    @objc func middleView3Tapped() {
+        print("middleView3 a été tapé")
+        // Ajoutez ici le code pour gérer le tap sur middleView3
+    }
+    
+    @objc func middleView4Tapped() {
+        print("middleView4 a été tapé")
+        // Ajoutez ici le code pour gérer le tap sur middleView4
     }
     
     func resetLayers() {
@@ -92,4 +132,13 @@ class ViewController: UIViewController {
         plusMiddleViewBottom.layer.opacity = 1
     }
     
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
 }
